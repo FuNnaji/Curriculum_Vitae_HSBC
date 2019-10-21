3c9e9c7e726e27d7fba53924da2a7035e737682c
Farouk Nnajiofor <fnnajiofor@atimi.com>
2019-10-21 11:51:47 -0700

    Initial Commit

---
+++ Curriculum Vitae Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Curriculum Vitae Test
+//
+//  Created by Farouke Nnajiofor on 2019-10-21.
+//  Copyright © 2019 Farouke Nnajiofor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
