347e0f3cddcaff62fbab4224051878af499ad8f9
Farouk Nnajiofor <fnnajiofor@atimi.com>
2019-10-21 17:38:54 -0700

    Bug fixes in unit test

--- Curriculum Vitae TestTests/Curriculum_Vitae_TestTests.swift
+++ Curriculum Vitae TestTests/Curriculum_Vitae_TestTests.swift
@@ -12,11 +12,25 @@ import XCTest
 class Curriculum_Vitae_TestTests: XCTestCase {
 
     func testParsingCV() {
+        
+        var testVitae : Vitae?
+        var testErrorString : String?
+        
+        let parsingExpectation = expectation(description: "Parsing CV Data...")
+        
         let asset = NSDataAsset(name: "dataset.json")
         DataController.parseCVData(data: asset!.data,
                                    completionHandler: {vitae in
-                                        XCTAssertEqual("Farouk Uzoma Nnajiofor", vitae.name)},
-                                   errorHandler: {_ in })
+                                        testVitae = vitae
+                                        parsingExpectation.fulfill()},
+                                   errorHandler: {errorString in
+                                        testErrorString = errorString
+                                        parsingExpectation.fulfill()})
+        
+        waitForExpectations(timeout: 3)
+        XCTAssertEqual("Farouk Uzoma Nnajiofor", testVitae?.name)
+        XCTAssertEqual(nil, testErrorString)
+        
     }
 
 }
